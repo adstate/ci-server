d5ae67c748625ab870826b4fa3579ef4682c4e47
Felix <pta_0704@mail.ru>
2020-04-24 13:32:59 +1000

    remove build folder

--- server/build/models/agentStatus.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var AgentStatus;
-(function (AgentStatus) {
-    AgentStatus["Waiting"] = "Waiting";
-    AgentStatus["Busy"] = "Busy";
-})(AgentStatus || (AgentStatus = {}));
-exports.default = AgentStatus;
